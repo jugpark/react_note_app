8d82732844c5b8982b58a0e35d1b01d019c41348
jugpark <hjjk0721@gmail.com>
2024-06-18 13:53:24 +0900

    feat: ErrorPage

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import {Container} from "./ErrorPage.styles.ts";
+import img from "../../assets/errorImg.png";
+import {ButtonFill} from "../../styles/styles.tsx";
+import {useNavigate} from "react-router-dom";
 
 const ErrorPage = () => {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <div className="error__img">
+                <img src={img} alt={"pageNotFound"}/>
+            </div>
+            <div className="error__text">
+                <h1>404</h1>
+                <div>Error Founded</div>
+                <ButtonFill onClick={() => {navigate('/')}}></ButtonFill>
+            </div>
+        </Container>
     );
 };
 
